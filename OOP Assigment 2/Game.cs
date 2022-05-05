bebbbc794f9a3d1f9b8cfa5744da720c21c0c42c
kleybson sousa <kleybson.sousa@hotmail.com>
2022-05-05 11:36:47 +0100

    Fixed the roll remaining method

--- OOP Assigment 2/Game.cs
+++ OOP Assigment 2/Game.cs
@@ -12,23 +12,39 @@ internal class Game
     public void DisplayScoreBoard()
     {
         Console.WriteLine("Game Board Score:");
-        Console.WriteLine("Player 1 has {0) points",player1.GetPoints());
-        Console.WriteLine("Player 2 has {0) points",player2.GetPoints());
-
+        Console.WriteLine("Player 1 has {0} points",player1.GetPoints());
+        Console.WriteLine("Player 2 has {0} points",player2.GetPoints());
+    }
+    public void DisplayDices( List<int> dices)
+    {
+        foreach (int dice in dices)
+        {
+            Console.Write(dice + " ");
+        }
+        Console.WriteLine("\n");
     }
     // Player vs Player
     public void PlayPVP()
     {
-        
 
+        player1.SetName();
+        player2.SetName();
         while ( player1.GetPoints() < 50 || player2.GetPoints() < 50)
         {
-            CheckforNumbers(player1.PlayDice(), player1);
+            CheckforNumbers(player1.PlayDice(), player1);            
             CheckforNumbers(player2.PlayDice(), player2);
 
             DisplayScoreBoard();
         }
-        
+        if ( player1.GetPoints() > player2.GetPoints())
+        {
+            Console.WriteLine("{0} has won the game with {1} points ",player1.GetName(),player1.GetPoints());
+        }
+        else
+        {
+            Console.WriteLine("{0} has won the game with {1} points ", player2.GetName(), player2.GetPoints());
+        }
+
 
     }
     public void CheckforNumbers(List<int> diceValues, Player player)
@@ -54,6 +70,7 @@ internal class Game
                     }
                     break;                    
                 }
+                
                 diceValues = player.PlayRemainingDices(diceValues, NofKind);
                 if (diceValues.Count <= 0)
                 {
@@ -63,6 +80,7 @@ internal class Game
                 }
                 else
                 {
+                    DisplayDices(diceValues);
                     CheckforNumbers(diceValues, player);
                 }
                 break;
--- OOP Assigment 2/Player.cs
+++ OOP Assigment 2/Player.cs
@@ -15,7 +15,7 @@ internal class Player
     Die[] die = new Die[5];
     Random random = new Random();
 
-    public void GetName()
+    public void SetName()
     {
         string name = string.Empty;
 
@@ -24,14 +24,20 @@ internal class Player
             Console.WriteLine("Please enter your name: ");
             name = Console.ReadLine();
         }
+        Name = name;
+    }
+
+    public string GetName()
+    {
+        return Name;
     }
     public List<int> PlayDice()
     {
-
-
         List<int> diceValues = new List<int>();
-        Console.WriteLine("Please press 'Enter' to roll your dices");
-        Console.WriteLine( "rolling dices Dices");
+        Console.WriteLine("{0} Please press 'Enter' to roll your dices",Name);
+        Console.ReadKey();
+        Console.WriteLine( "{0} rolling dices Dices", Name);
+        Thread.Sleep(3000);
         //for (int i = 0; i < 5; i++)
         //{
         //    int selectNum = die.Numbers[random.Next(0, die.Numbers.Length - 1)];
@@ -47,41 +53,45 @@ internal class Player
 
         Console.WriteLine("The dices have been rolled, those are the values you got: ");
         foreach (int diceValue in diceValues) { Console.Write(diceValue + " "); }
-        
+        Console.WriteLine("\n");
         return diceValues;
     }
     public List<int> PlayRemainingDices(List<int> diceValues, int[] NofKind)
     {
-        //int rounds = 2;
         int selectNum;
         List<int> dicesToBeRolled = new List<int>();
-        for (int i = 0; i < diceValues.Count; i++) 
+        if (rounds > 0)
         {
-            if ( diceValues[i] != NofKind[0])
-            {   dicesToBeRolled.Add(i); 
-                rounds--;
-            }
-        }
-        foreach (int i in dicesToBeRolled)
-        {
-            selectNum = die[i].Numbers[random.Next(0, die[i].Numbers.Length - 1)];
-            die[i].SetValueOnTop(selectNum);
-        }
-
-        diceValues.Clear();
-        
-        if ( rounds > 0)
-        {
-            for (int i = 0; i < die.Length; i++)
+            Console.WriteLine("You have not scored any points, however you have 2 of a kind, press enter to play the remaining dices again");
+            Console.ReadKey();
+            for (int i = 0; i < diceValues.Count; i++)
             {
-                diceValues.Add(die[i].GetValueOnTop());
+                if (diceValues[i] != NofKind[0])
+                {
+                    dicesToBeRolled.Add(i);
+                }
             }
-            return diceValues;
-        }        
+            foreach (int i in dicesToBeRolled)
+            {
+                selectNum = die[i].Numbers[random.Next(0, die[i].Numbers.Length - 1)];
+                die[i].SetValueOnTop(selectNum);
+            }
+
+            diceValues.Clear();
+
+            if (rounds > 0)
+            {
+                for (int i = 0; i < die.Length; i++)
+                {
+                    diceValues.Add(die[i].GetValueOnTop());
+                }
+                rounds--;
+                return diceValues;
+            }
+        }
+        diceValues.Clear();
         return diceValues;
     }
-
-
     public void SetPoints(int points)
     {
         Points += points;
@@ -90,6 +100,5 @@ internal class Player
     {
         return Points;
     }
-
 }
 
